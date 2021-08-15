07160ea3fc213e3d8767b75c06330acc9f788f76
SuperWebMan <aleksandrshreider@outlook.com>
2021-08-16 03:22:52 +0900

    Modify for using own Router and Factory

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,7 +4,7 @@ import { AbstractConnector } from '@web3-react/abstract-connector'
 // import { bsc, fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
 import { injected, bsc } from '../connectors'
 // TODO
-export const ROUTER_ADDRESS = '0x10ED43C718714eb63d5aA57B78B54704E256024E'
+export const ROUTER_ADDRESS = '0x948b3adfc795Cc4144D061a9e69fcA4C01E4e62D'
 
 // a list of tokens by chain
 type ChainTokenList = {

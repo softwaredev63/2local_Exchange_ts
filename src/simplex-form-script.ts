b686c849bba9fad213eb575bc98b197fd4be135a
SuperWebMan <aleksandrshreider@outlook.com>
2021-09-14 23:28:14 +0900

    Update simplex live

--- src/simplex-form-script.ts
+++ src/simplex-form-script.ts
@@ -2,7 +2,8 @@ export const loadSimplexForm = (callback) => {
     const simplexFormScript = document.getElementById('simplexForm');
     if (!simplexFormScript) {
         const script = document.createElement('script');
-        script.src = 'https://iframe.sandbox.test-simplexcc.com/form-sdk.js';
+        // script.src = 'https://iframe.sandbox.test-simplexcc.com/form-sdk.js';
+        script.src = 'https://iframe.simplex-affiliates.com/form-sdk.js';
         script.id = 'simplexForm';
         document.body.appendChild(script);
         script.onload = () => {

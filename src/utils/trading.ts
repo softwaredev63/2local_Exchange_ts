bbb6deae3f64168fd474da2fe44e0aa399ea8dfb
Alexey Shalin <shalin.2001@bk.ru>
2021-10-25 09:02:27 +0700

    Approve BUSD to decrease gas fee on deposit

--- src/utils/trading.ts
+++ src/utils/trading.ts
@@ -1,18 +1,26 @@
 import { parseEther } from '@ethersproject/units';
 import { getContract } from './index';
 import TradingContractABI from '../constants/abis/TradingContract.json';
+import BUSDABI from '../constants/abis/BUSD.json';
 
 export async function Deposit(provider, userAddress, amount/* BUSD amount */) {
     const tradingContractAddress: string = process.env.REACT_APP_TRADING_CONTRACT_ADDRESS!;
-
-    const tradingContract = getContract(tradingContractAddress, TradingContractABI, provider, userAddress);
+    const busdAddress: string = process.env.REACT_APP_BUSD_ADDRESS!;
 
     const depositAmount = parseEther(`${amount}`); 
+
+    // 1. First approve BUSD
+    const busdContract = getContract(busdAddress, BUSDABI, provider, userAddress);
+    const txApprove = await busdContract.approve(tradingContractAddress, depositAmount);
+    await txApprove.wait();
+
+    // 2. Next deposit BUSD
+    const tradingContract = getContract(tradingContractAddress, TradingContractABI, provider, userAddress);
 
     const txDeposit = await tradingContract.deposit(depositAmount, {
         from: userAddress
     });
-    return await txDeposit.wait()
+    return await txDeposit.wait();
 }
 
 export async function Withdraw(provider, userAddress, amount/* 2LCT amount */) {

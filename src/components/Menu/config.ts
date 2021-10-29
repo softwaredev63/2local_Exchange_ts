9bfc273c900693c5fdb111d6c88b958882d0bf27
SuperWebMan <aleksandrshreider@outlook.com>
2021-10-29 18:11:34 +0900

    Change logo image and enable trading menu

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -70,9 +70,9 @@ const config: MenuEntry[] = [
       },
       {
         label: 'Trading Pools',
-        href: '/trading',
+        href: 'https://trading.2local.io',
         icon: 'BETH2LCIcon',
-        disabled: true,
+        disabled: false,
       },
       {
         label: 'Staking Pools',
